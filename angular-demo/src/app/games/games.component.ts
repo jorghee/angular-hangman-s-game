df86e0dd964de56bee6566d9d5cac3416ecfac95
Jorghee <jorgeehuarsaya@gmail.com>
2024-06-20 17:27:05 -0500

    Pasando información de padre a hijo.

--- angular-demo/src/app/games/games.component.ts
+++ angular-demo/src/app/games/games.component.ts
@@ -1,28 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UserComponent } from '../user/user.component';
 
 @Component({
   selector: 'app-games',
   standalone: true,
-  imports: [],
+  imports: [UserComponent],
   templateUrl: './games.component.html',
   styleUrl: './games.component.css'
 })
 export class GamesComponent {
+  // Uso del decorador, este component se comporta como hijo
+  @Input() username: string = '';
+
   games = [
     {
-      id: 1;
+      id: 1,
       name: 'Fortnite'
     },
     {
-      id: 2;
+      id: 2,
       name: 'Left 4 Dead 2'
     },
     {
-      id: 3;
-      name: 'Valorant'
+      id: 3,
+      name: 'God of War 2'
     },
     {
-      id: 4;
+      id: 4,
       name: 'Vice City'
     }
   ]
--- angular-demo/src/app/user/user.component.ts
+++ angular-demo/src/app/user/user.component.ts
@@ -10,5 +10,9 @@ import { GamesComponent } from '../games/games.component';
 })
 export class UserComponent {
   username = 'Jorge';
-  isLoggedIn = true;
+  isLoggedIn = false;
+
+  greet() {
+    alert("Hello!");
+  }
 }
